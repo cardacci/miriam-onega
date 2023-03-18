d7915b0c00b529eea43feec4b47fcf6848bc600c
Gabriel Cardacci <gabrielcardacci@gmail.com>
2023-03-18 17:00:54 -0300

    Adding information in the book "Being Resilient"

--- pages/books/ser-resiliente.jsx
+++ pages/books/ser-resiliente.jsx
@@ -11,15 +11,43 @@ export default function SerResiliente() {
 			/>
 
 			<div className='page-subtitle'>
-				[TEXTO CONTRATAPA]
+				La autora a través de su novela autobiográfica con sus metáforas, desea llevar un mensaje positivo a la sociedad; a pesar de todos los obstáculos y dolores que conlleva vivir, se pueden superar los mismos, ser una mejor persona, irradiando luz infinita para consigo y los dempas, SER un SER RESILIENTE.
 			</div>
 
-			<Image
-				alt='Ser Resiliente'
-				height={250}
-				src='/books/ser-resiliente/ser-resiliente-animacion.gif'
-				width={250}
-			/>
+			<div
+				className='text-quote'
+				style={{ marginTop: '20px' }}
+			>
+				<p>
+					&quot;Somos todos libres de hacer lo que queramos, siempre que no perjudiquemos a los demás... recuerda de dónde has venido, adónde vas...&quot;
+				</p>
+
+				<p>
+					&quot;Enseñar es recordarles a los demás que saben tanto como tú, somos todos aprendices, maestros... Aprender es redescubrir lo que ya sabes...&quot;
+				</p>
+
+				<span>
+					&apos;Ilusiones&apos; Richard Bach
+				</span>
+			</div>
+
+			<div style={{ marginTop: '40px' }}>
+				<Image
+					alt='Ser Resiliente'
+					height={426.67}
+					src='/books/ser-resiliente/Ser Resiliente - Tapa.jpeg'
+					width={305.67}
+				/>
+			</div>
+
+			<div style={{ marginTop: '40px' }}>
+				<Image
+					alt='Ser Resiliente'
+					height={426.67}
+					src='/books/ser-resiliente/Ser Resiliente - Contratapa.jpeg'
+					width={305.67}
+				/>
+			</div>
 		</div>
 	)
 }
